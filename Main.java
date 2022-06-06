5349cecaf2d3aee10a58a481a2a2f1d25bd06c49
EveGarayalde <106777418+EveGarayalde@users.noreply.github.com>
2022-06-06 16:09:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+
 package main;
 
 import mod.Overseer;
 
 public class Main {
+	
+	//This method is used to create a new Overseer object that runs the program.
 	public static void main(String[] args0) {
 		Overseer over = new Overseer();
 	}
